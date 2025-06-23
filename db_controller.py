f1ffcb96183206208966a83a36093d871776712b
Armeen Ghoorkhanian <armeeng@yahoo.com>
2025-06-23 13:05:40 -0400

    nothing

--- db_controller.py
+++ db_controller.py
@@ -33,7 +33,7 @@ def main(
     session = Session()
 
     current = start_date
-    current = date(2023, 10, 8)
+    current = date(2023, 8, 12)
     while current >= stop_date:
         for sport in SportEnum:
             # see if we've already done this sport/date
